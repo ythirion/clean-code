0615c83219a62ddf5df77c12b9e94fc9118e76c7
Yoan Thirion <yoan.thirion@gmail.com>
2020-10-23 17:50:53 +0200

    Exercise 2 : use StringBuilder + rename

--- src/main/java/Exercise2.java
+++ src/main/java/Exercise2.java
@@ -2,14 +2,16 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Exercise2 {
-    public String transform(List<LocalDate> s) {
-        String d = null;
-        for (LocalDate kc : s) // arrival times {
-            if (kc.isAfter(LocalDate.now())) {
-                d = d + kc + "\n";
+    public String toArrivalTable(List<LocalDate> arrivalTimes) {
+        StringBuilder arrivalTable = new StringBuilder();
+
+        for (LocalDate flightArrival : arrivalTimes) {
+            if (flightArrival.isAfter(LocalDate.now())) {
+                arrivalTable.append(flightArrival + "\n");
             } else {
-                d = d + "Delayed\n";
-            } // flight is delayed
-        return d;
+                arrivalTable.append("Delayed\n");
+            }
+        }
+        return arrivalTable.toString();
     }
 }
