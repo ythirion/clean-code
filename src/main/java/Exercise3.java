d484afadeb941452288ca3fb93d6ca45d6c21261
Yoan Thirion <yoan.thirion@gmail.com>
2020-10-26 10:15:41 +0100

    Exercise 3 : SRP > extract Customer + rm switch

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -1,43 +1,23 @@
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
 public class Exercise3 {
 
     @AllArgsConstructor
     public class DiscountCalculator {
-        public static final double DISCOUNT_FOR_SIMPLE_CUSTOMERS = 0.1;
-        public static final double DISCOUNT_FOR_VALUABLE_CUSTOMERS = 0.3;
-        public static final double DISCOUNT_FOR_MOST_VALUABLE_CUSTOMERS = 0.5;
-
         private final LoyaltyDiscountCalculator loyaltyDiscountCalculator;
+        private final CustomerDiscountCalculator customerDiscountCalculator;
 
         public double calculate(double price,
                                 CustomerStatus customerStatus,
                                 int yearsSinceAccountCreation) {
-            double priceAfterDiscount = 0;
+            double discountForCustomerStatus = customerDiscountCalculator.calculateDiscount(customerStatus);
             double discountForLoyalty = loyaltyDiscountCalculator.calculateDiscount(yearsSinceAccountCreation);
 
-            switch (customerStatus) {
-                case NOT_REGISTERED:
-                    priceAfterDiscount = price;
-                    break;
-                case SIMPLE:
-                    priceAfterDiscount = calculatePriceAfterDiscount(price, discountForLoyalty, DISCOUNT_FOR_SIMPLE_CUSTOMERS);
-                    break;
-                case VALUABLE:
-                    priceAfterDiscount = calculatePriceAfterDiscount(price, discountForLoyalty, DISCOUNT_FOR_VALUABLE_CUSTOMERS);
-                    break;
-                case MOST_VALUABLE:
-                    priceAfterDiscount = calculatePriceAfterDiscount(price, discountForLoyalty, DISCOUNT_FOR_MOST_VALUABLE_CUSTOMERS);
-                    break;
-            }
-            return priceAfterDiscount;
-        }
-
-        private double calculatePriceAfterDiscount(double price,
-                                                   double discountForLoyalty,
-                                                   double discountForCustomer) {
-            double priceAfterDiscount = (price - (discountForCustomer * price));
+            double priceAfterDiscount = (price - (discountForCustomerStatus * price));
             return priceAfterDiscount - (discountForLoyalty * priceAfterDiscount);
         }
     }
@@ -47,6 +27,25 @@ public class Exercise3 {
 
         private double calculateDiscount(int yearsSinceAccountCreation) {
             return (yearsSinceAccountCreation > 5) ? MAXIMUM_DISCOUNT_FOR_LOYALTY / 100 : yearsSinceAccountCreation / 100;
+        }
+    }
+
+    public class CustomerDiscountCalculator {
+        private static final double DISCOUNT_FOR_SIMPLE_CUSTOMERS = 0.1;
+        private static final double DISCOUNT_FOR_VALUABLE_CUSTOMERS = 0.3;
+        private static final double DISCOUNT_FOR_MOST_VALUABLE_CUSTOMERS = 0.5;
+
+        private final HashMap<CustomerStatus, Double> discountsForCustomer = new HashMap<CustomerStatus, Double>() {{
+            put(CustomerStatus.NOT_REGISTERED, 0.0);
+            put(CustomerStatus.SIMPLE, DISCOUNT_FOR_SIMPLE_CUSTOMERS);
+            put(CustomerStatus.VALUABLE, DISCOUNT_FOR_VALUABLE_CUSTOMERS);
+            put(CustomerStatus.MOST_VALUABLE, DISCOUNT_FOR_MOST_VALUABLE_CUSTOMERS);
+        }};
+
+        private double calculateDiscount(CustomerStatus customerStatus) {
+            if (!discountsForCustomer.containsKey(customerStatus))
+                throw new NoSuchElementException();
+            return discountsForCustomer.get(customerStatus);
         }
     }
 
