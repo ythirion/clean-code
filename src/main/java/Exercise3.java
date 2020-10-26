09488362048812e9f54745a667d8396d9e4c09c6
Yoan Thirion <yoan.thirion@gmail.com>
2020-10-26 09:29:18 +0100

    Exercise 3 : introduce switch case

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -8,14 +8,19 @@ public class Exercise3 {
             double priceAfterDiscount = 0;
             double disc = (years > 5) ? (double) 5 / 100 : (double) years / 100;
 
-            if (customerStatus == CustomerStatus.NOT_REGISTERED) {
-                priceAfterDiscount = amount;
-            } else if (customerStatus == CustomerStatus.SIMPLE) {
-                priceAfterDiscount = (amount - (0.1 * amount)) - disc * (amount - (0.1 * amount));
-            } else if (customerStatus == CustomerStatus.VALUABLE) {
-                priceAfterDiscount = (0.7 * amount) - disc * (0.7 * amount);
-            } else if (customerStatus == CustomerStatus.MOST_VALUABLE) {
-                priceAfterDiscount = (amount - (0.5 * amount)) - disc * (amount - (0.5 * amount));
+            switch (customerStatus) {
+                case NOT_REGISTERED:
+                    priceAfterDiscount = amount;
+                    break;
+                case SIMPLE:
+                    priceAfterDiscount = (amount - (0.1 * amount)) - disc * (amount - (0.1 * amount));
+                    break;
+                case VALUABLE:
+                    priceAfterDiscount = (0.7 * amount) - disc * (0.7 * amount);
+                    break;
+                case MOST_VALUABLE:
+                    priceAfterDiscount = (amount - (0.5 * amount)) - disc * (amount - (0.5 * amount));
+                    break;
             }
             return priceAfterDiscount;
         }
