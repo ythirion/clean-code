9cf5c7d3b57422e96a061247a127d6216130289e
Yoan Thirion <yoan.thirion@gmail.com>
2020-10-26 09:27:47 +0100

    Exercise 3 : Remove useless comments+extract enum

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -1,38 +1,34 @@
-public class Exercise3 {
-    /**
-     *
-     * @param amount price
-     * @param type Account status (NotRegistered = 1, SimpleCustomer = 2, ValuableCustomer = 3, MostValuableCustomer = 4)
-     * @param years Time in years that the client is our customer
-     * @return THE PRICE
-     */
-    public double calculate(double amount, int type, int years)
-    {
-        double result = 0;
-        double disc = (years > 5) ? (double) 5 / 100 : (double)years / 100;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-        // NotRegistered
-        if (type == 1)
-        {
-            result = amount;
-            return result;
+public class Exercise3 {
+
+    public class DiscountManager {
+        public double calculate(double amount, CustomerStatus customerStatus, int years) {
+            double priceAfterDiscount = 0;
+            double disc = (years > 5) ? (double) 5 / 100 : (double) years / 100;
+
+            if (customerStatus == CustomerStatus.NOT_REGISTERED) {
+                priceAfterDiscount = amount;
+            } else if (customerStatus == CustomerStatus.SIMPLE) {
+                priceAfterDiscount = (amount - (0.1 * amount)) - disc * (amount - (0.1 * amount));
+            } else if (customerStatus == CustomerStatus.VALUABLE) {
+                priceAfterDiscount = (0.7 * amount) - disc * (0.7 * amount);
+            } else if (customerStatus == CustomerStatus.MOST_VALUABLE) {
+                priceAfterDiscount = (amount - (0.5 * amount)) - disc * (amount - (0.5 * amount));
+            }
+            return priceAfterDiscount;
         }
-        // SimpleCustomer
-        else if (type == 2)
-        {
-            result = (amount - (0.1 * amount)) - disc * (amount - (0.1 * amount));
-        }
-        // ValuableCustomer
-        else if (type == 3)
-        {
-            result = (0.7 * amount) - disc * (0.7 * amount);
-            return result;
-        }
-        // MostValuableCustomer
-        else if (type == 4)
-        {
-            result = (amount - (0.5 * amount)) - disc * (amount - (0.5 * amount));
-        }
-        return result;
+    }
+
+    @AllArgsConstructor
+    public enum CustomerStatus {
+        NOT_REGISTERED(1), // all these enums give error, for no constructor
+        SIMPLE(2),
+        VALUABLE(3),
+        MOST_VALUABLE(4);
+
+        @Getter
+        private final int value;
     }
 }
