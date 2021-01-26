7367fbc322df49a8bc39bdd95332f2e112818130
Yoan Thirion <yoan.thirion@gmail.com>
2021-01-26 14:09:03 +0100

    Improved Stream Solution

--- src/main/java/Exercise2.java
+++ src/main/java/Exercise2.java
@@ -5,8 +5,7 @@ import java.util.stream.Collectors;
 public class Exercise2 {
     public String toArrivalTable(List<LocalDate> arrivalTimes) {
         return arrivalTimes.stream()
-                .map(flightArrival ->
-                        flightArrival.isAfter(LocalDate.now()) ? flightArrival + "\n" : "Delayed\n")
-                .collect(Collectors.joining());
+                .map(flightArrival -> flightArrival.isAfter(LocalDate.now()) ? flightArrival.toString() : "Delayed")
+                .collect(Collectors.joining("\n"));
     }
 }
